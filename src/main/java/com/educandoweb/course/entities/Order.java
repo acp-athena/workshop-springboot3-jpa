3eecd4ac4ae3e0622f7e95c35d58145df96e314e
acp-athena <anacpaula83@gmail.com>
2025-03-28 15:52:48 -0300

    OrderStatus Enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -28,12 +29,22 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 	
 	public Order() {
 		
+	}
+	
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		Id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
 	}
 
 	public Long getId() {
@@ -51,6 +62,15 @@ public class Order implements Serializable {
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null)
+			this.orderStatus = orderStatus.getCode();
+	}
 
 	public User getClient() {
 		return client;
@@ -60,12 +80,6 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
-	public Order(Long id, Instant moment, User client) {
-		super();
-		Id = id;
-		this.moment = moment;
-		this.client = client;
-	}
 
 	@Override
 	public int hashCode() {
